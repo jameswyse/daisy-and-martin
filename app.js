591e5af7870fda3138dfd56690eebf8547a80237
James Wyse <james@jameswyse.net>
2014-03-18 14:15:04 +1000

    Tidy

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var moment = require('moment');
-
-var app   = require('./lib/app');
-var email = require('./lib/email');
+var app    = require('./lib/app');
+var email  = require('./lib/email');
 
 app.web.get('/', function(req, res, next) {
   var remaining = moment([2014, 6, 5]).fromNow();
+
   res.render('pages/home', { page: 'wedding', remaining: remaining });
 });
 
@@ -24,15 +24,7 @@ app.web.get('/rsvp', function(req, res, next) {
   res.render('pages/rsvp', { page: 'rsvp' });
 });
 
-app.web.post('/rsvp', email.rsvp, function(req, res, next) {
-  res.render('pages/rsvp', { page: 'rsvp' });
-});
-
 app.web.get('/contact', function(req, res, next) {
-  res.render('pages/contact', { page: 'contact' });
-});
-
-app.web.post('/contact', email.contact, function(req, res, next) {
   res.render('pages/contact', { page: 'contact' });
 });
 
@@ -40,6 +32,16 @@ app.web.get('/photos', function(req, res, next) {
   res.render('pages/photos', { page: 'photos' });
 });
 
+//
+// Form Handlers
+//
+app.web.post('/contact', email.contact, function(req, res, next) {
+  res.render('pages/contact', { page: 'contact' });
+});
+
+app.web.post('/rsvp', email.rsvp, function(req, res, next) {
+  res.render('pages/rsvp', { page: 'rsvp' });
+});
 
 
 app.start();
