44c63dd16454be468e19fa9a5d6f143b17b26d6b
James Wyse <james@jameswyse.net>
2014-03-26 03:15:50 +1000

    Preload images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,27 @@
 var $ = require('jquery/dist/jquery');
 
+function preload(url, selector) {
+  $('<img/>')
+    .attr({
+      src: url,
+      height: '1',
+      width: '1'
+    })
+    .css({
+      opacity: '0.1'
+    })
+    .appendTo($(selector || 'body'))
+    .delay(1000)
+    .remove();
+}
+
 $(function() {
+
+  preload('/images/icon-radio@2x.png');
+  preload('/images/icon-tick@2x.png');
+  preload('/images/icon-arrow.png');
+  preload('/images/icon-cross@2x.png');
+
   var $menu = $('.menu');
 
   $menu.click(function(e) {
